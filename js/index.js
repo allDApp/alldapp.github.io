c32b3f65946b06f7d2b0644e34271c17fe09597c
Antiky <31079363+Antiky@users.noreply.github.com>
2018-05-30 17:09:34 +0800

    fix resp

--- js/index.js
+++ js/index.js
@@ -191,9 +191,14 @@ function save(){
 		var callFunction = "savedapp";
 		var callArgs = "[\"" + name + "\u003cbr\u003e地址:http://alldapp.github.io/" + ids + "/\u003cbr\u003e描述:" + content + "\"]";
 		nebpay.call(to, value, callFunction, callArgs, {
-			listener: function(resp) {
-				console.log(JSON.stringify(resp));
-				alert("发送成功");
+			listener: function Push(resp) {
+				console.log("response of push: " + JSON.stringify(resp))
+				var respString = JSON.stringify(resp);
+				if(respString.search("rejected by user") !== -1){
+					alert("关闭交易,取消上传你的Dapp信息")
+				}else if(respString.search("txhash") !== -1){
+					alert("上传Hash: " + resp.txhash+"请等待交易确认,如果上传失败请检查内容是否含有特殊字符")
+				}
 			}
 		});
 	
